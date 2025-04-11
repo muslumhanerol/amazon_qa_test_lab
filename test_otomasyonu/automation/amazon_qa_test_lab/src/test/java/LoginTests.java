6b8fffd1598f855eff3a364a2a551f8339649baa
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2025-04-11 22:00:18 +0300

    Update LoginTests.java

--- test_otomasyonu/automation/amazon_qa_test_lab/src/test/java/LoginTests.java
+++ test_otomasyonu/automation/amazon_qa_test_lab/src/test/java/LoginTests.java
@@ -9,7 +9,7 @@ public class LoginTests {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.amazon.com.tr/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com.tr%2Fref%3Dnav_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=trflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0");
-        driver.findElement(By.id("ap_email")).sendKeys("kafolik297@provko.com");
+        driver.findElement(By.id("ap_email")).sendKeys("hopami8062@ptiong.com");
         driver.findElement(By.cssSelector("[class='a-button-input']")).click();
     }
 }
