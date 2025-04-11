75e334ad54dddfe107cf71b561a5060148b7e787
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2025-04-11 23:15:40 +0300

    Update LoginTests.java

--- test_otomasyonu/automation/amazon_qa_test_lab/src/test/java/LoginTests.java
+++ test_otomasyonu/automation/amazon_qa_test_lab/src/test/java/LoginTests.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 
 public class LoginTests {
     @Test
-    public void basariliGiris(){
+    public void basariliGiris() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.amazon.com.tr/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com.tr%2Fref%3Dnav_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=trflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0");
@@ -13,5 +13,11 @@ public class LoginTests {
         driver.findElement(By.cssSelector("[class='a-button-input']")).click();
         driver.findElement(By.id("ap_password")).sendKeys("AMAZONDENEME");
         driver.findElement(By.cssSelector("[class='a-button-input']")).click();
+
+        String text = driver.findElements(By.cssSelector("[class='nav-line-2 ']")).get(0).getText();
+        System.out.println(text);
+
+
+        driver.quit();
     }
 }
